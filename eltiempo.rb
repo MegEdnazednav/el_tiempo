c23d5bf0cb1f77e353b1039d6c7674ee0092597a
Meg Vandezande <meg.vandezande@gmail.com>
2020-01-25 20:50:29 +0100

    Splitting controller and parser

--- eltiempo.rb
+++ eltiempo.rb
@@ -1,76 +1,31 @@
-#!/usr/bin/env ruby
-class WeatherParser
+require_relative "./weather_parser"
 
-  require 'rest-client'
-  require 'nokogiri'
-  require 'byebug'
+class ReportsController
 
-  TYPES = { min: "Minimum Temperature",
-            max: "Maximum Temperature" }.freeze
-
-  def initialize(report_type, location_name)
+  def initialize(report_type, location_name, weather_parser)
     @report_type = report_type
     @location_name = location_name
+    @weather_parser = weather_parser
   end
 
   def call
-    set_location
+    location_id = weather_parser.get_location_id(location_name)
     case report_type
     when "-today"
-      set_today_weather
+      p weather_parser.get_today_weather(location_id)
     when "-av_max"
-      set_average_temperature("max")
+      answer = weather_parser.get_average_temperature("max", location_id)
+      p "This week's average #{answer[:type]} in #{location_name} is #{answer[:temperature]}°C"
     when "-av_min"
-      set_average_temperature("min")
+      answer = weather_parser.get_average_temperature("min", location_id)
+      p "This week's average #{answer[:type]} in #{location_name} is #{answer[:temperature]}°C"
     end
   end
 
   private
 
-  attr_reader :report_type, :location_name, :location_id, :average
-
-  def set_location
-    location_file = RestClient.get 'http://api.tiempo.com/index.php?api_lang=en&division=102&affiliate_id=1f95bxk4cjbu'
-    xml_location_file = Nokogiri::XML(location_file)
-
-    xml_location_file.search('//name').each do |name|
-      if (name.text == location_name)
-        @location_id = name['id'].to_i
-      end
-    end
-  end
-
-  def set_average_temperature(type)
-    average_file = RestClient.get "http://api.tiempo.com/index.php?api_lang=en&localidad=#{location_id}&affiliate_id=1f95bxk4cjbu"
-    xml_average_file = Nokogiri::XML(average_file)
-    xml_average_file.search('//name').each do |name|
-      if (name.text == TYPES[type.to_sym])
-        calculate_average(name)
-      end
-    end
-    p "This week's average #{TYPES[type.to_sym].downcase} in #{location_name} is #{average}°C"
-  end
-
-  def calculate_average(name)
-    average = []
-    name.parent.xpath('data/forecast').map{ |data| average << data['value'].to_i }
-    @average = (average.reduce(:+) / average.length).to_i
-  end
-
-  def set_today_weather
-    today_file = RestClient.get "http://api.tiempo.com/index.php?api_lang=en&localidad=#{location_id}&affiliate_id=1f95bxk4cjbu"
-    xml_today_file = Nokogiri::XML(today_file)
-
-    xml_today_file.search('//name').each do |name|
-      case name.text
-      when "Weather Symbol"
-        puts name.parent.xpath('data/forecast')[0]['value']
-      when "Wind"
-        puts name.parent.xpath('data/forecast')[0]['value']
-      end
-    end
-  end
+  attr_reader :report_type, :location_name, :weather_parser
 end
 
-parser = WeatherParser.new(ARGV[0], ARGV[1])
+parser = ReportsController.new(ARGV[0], ARGV[1], WeatherParser.new)
 parser.call
